b19fb96ae9fc88e6c05827097f19e0762f887604
ttjohn13 <ttjohn13@asu.edu>
2023-11-04 16:25:10 -0700

    progress on interpolation of slices

--- Slicing_and_Interp.py
+++ Slicing_and_Interp.py
@@ -3,13 +3,26 @@ import scipy as sp
 
 
 def rad2CartOffset(az, r, elv):
-    xoffset =
+    """
+    Calculate cartesian offset from measuring point
+    :param az: nx1 matrix of azimuthal degree angles
+    :param r: nx1 matrix of radial distances
+    :param elv: nx1 matrix of elevation angles from ground plane
+    :return: nx3 matrix of cartesian offsets
+    """
+    z_offset = np.multiply(np.sin(np.deg2rad(elv)), r)
+    x_offset = np.multiply(np.multiply(np.cos(np.deg2rad(elv)), np.cos(np.deg2rad(az))), r)
+    y_offset = np.multiply(np.multiply(np.cos(np.deg2rad(elv)), np.sin(np.deg2rad(az))), r)
+    cartesian_offset = np.hstack((x_offset, y_offset, z_offset))
+    return cartesian_offset
 
-def slice_points(data, x, y, z, az, rang, elev, number_of_measurements=100, lidar_loc=np.NAN):
+
+def slice_points(data, x, y, z, az, elev, lidar_loc=np.NAN):
     udata = data[:, :, :, 0]
     vdata = data[:, :, :, 1]
+    rang = np.divide(z, np.sin(np.deg2rad(elev)))
 
-    uinterp = sp.interpolate.RegularGridInterpolator((x, y, z,), udata)
+    uinterp = sp.interpolate.RegularGridInterpolator((x, y, z), udata)
     vinterp = sp.interpolate.RegularGridInterpolator((x, y, z), vdata)
 
     if np.isnan(lidar_loc):
@@ -18,3 +31,14 @@ def slice_points(data, x, y, z, az, rang, elev, number_of_measurements=100, lida
         yloc = max(y) / 2
         lidar_loc = np.array([xloc, yloc, zloc])
 
+    offsets = np.zeros((len(rang), len(az), 3))
+    for i in range(len(rang)):
+        offsets[i, :, :] = rad2CartOffset(az, rang[i], elev)
+
+    locations = lidar_loc + offsets
+    u_interp_val = uinterp(locations)
+    v_interp_val = vinterp(locations)
+    x_return = locations[:, 0]
+    y_return = locations[:, 1]
+    z_return = locations[:, 2]
+    return u_interp_val, v_interp_val, x_return, y_return, z_return
