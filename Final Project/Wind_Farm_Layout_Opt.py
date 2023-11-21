954e3419ad853a3d95588f1fcbf6f18600efed7d
ttjohn13 <ttjohn13@asu.edu>
2023-11-20 18:15:45 -0700

    Starting to build GA

--- Final Project/Wind_Farm_Layout_Opt.py
+++ Final Project/Wind_Farm_Layout_Opt.py
@@ -1,2 +1,12 @@
 import numpy as np
 import scipy as sp
+import pygad as pg
+
+gene_space = [0, 1]  # possible values for each gene space
+
+target_number_of_turbines = 15
+
+def fitness_func(ga_instance, solution, solution_idx):
+    number_of_turbines = np.sum(solution)
+    turbine_count_error = np.abs(target_number_of_turbines-number_of_turbines)
+
