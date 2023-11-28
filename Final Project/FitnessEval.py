eb344570c2cca70ad25c2e3f6244d3fc83ded6e0
ttjohn13 <ttjohn13@asu.edu>
2023-11-27 21:58:21 -0700

    Ordering of coordinates needs some work

--- Final Project/FitnessEval.py
+++ Final Project/FitnessEval.py
@@ -1,4 +1,13 @@
 import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class CoordNOrder:
+    coord: np.ndarray
+    order: int
+
+
 
 def individualCreation(num_ones, y_size, x_size):
     total_elements = y_size * x_size
@@ -24,7 +33,36 @@ def turbinelocate(grid, x_grid, y_grid):
     return turbine_coord
 
 
+def turbineorder(turbine_coord):
+    ordered_coord = np.sort(turbine_coord, axis=0)
+    loc_and_order = []
+
+    for i in range(len(ordered_coord[:, 0])):
+        coordinate = ordered_coord[i, :].reshape((1, 2))
+        if i>0:
+            previous_coord = loc_and_order[i-1].coord
+            previous_order = loc_and_order[i-1].order
+
+            if previous_coord[0, 0] < coordinate[0, 0]:
+                current_order = previous_order + 1
+            elif previous_coord[0, 0] == coordinate[0, 0]:
+                current_order = previous_order
+            else:
+                raise Exception("X coordinate not greater or equal to previous coordinate")
+
+        else:
+            current_order = 1
+
+        current_coord_and_order = CoordNOrder(coordinate, current_order)
+        loc_and_order.append(current_coord_and_order)
+
+    return loc_and_order
+
+
+
+
 individual_1 = individualCreation(8, 5, 5)
 
 grid_1, x_grid_1, y_grid_1 = gridcreate(individual_1, 5, 5, 52)
 turbine_locations = turbinelocate(grid_1, x_grid_1, y_grid_1)
+ordered_turbine_locations = turbineorder(turbine_locations)
